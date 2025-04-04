44129fe7b100c619ad6f74aa956b9568da851f84
shubhamrathee11 <160757279+shubhamrathee11@users.noreply.github.com>
2025-04-04 22:36:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import MainContent from './components/MainContent'
+import Sidebar from './components/Sidebar'
+
+function App() {
+
+  return (
+    <div className='flex'>
+      <Sidebar/>
+      <MainContent/>
+    </div>
+  )
+}
+
+export default App
