0027d06d74cc54e4a41da3e2fda80fa604ca78c9
shubhamrathee11 <160757279+shubhamrathee11@users.noreply.github.com>
2025-04-05 10:47:38 +0530

    Delete src/MobileSidebarTop.jsx

--- src/MobileSidebarTop.jsx
+++
@@ -1,9 +0,0 @@
-import icon from "../assets/icon.jpg";
-
-const MobileSidebarTop = () => (
-  <div className="md:hidden fixed top-4 left-4 z-50">
-    <img src={icon} alt="Logo" className="w-[25px] h-[25px] rounded-full" />
-  </div>
-);
-
-export default MobileSidebarTop;
